fcf27feb058a7b69e2ea74308e58693d27c4f522
mickhan <catmic27@gmail.com>
2020-05-13 16:15:57 +0800

    最高有效位

--- counting-bits/main.go
+++ counting-bits/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func countBits(num int) []int {
 	var res []int
@@ -16,6 +18,24 @@ func countBits(num int) []int {
 	return res
 }
 
+func countBits2(num int) []int {
+	var res []int
+	res = make([]int, num+1)
+	res[0] = 0
+	for i := 0; i <= num/2+1; i++ {
+		// 已知当前位置i的1数量为xi
+		// 所有i+2^m位置的2数量为xi+1（注意2^m要大于i）
+		prefix := 1
+		for i+prefix <= num {
+			if prefix > i {
+				res[i+prefix] = res[i] + 1
+			}
+			prefix = prefix * 2
+		}
+	}
+	return res
+}
+
 func main() {
-	fmt.Println(countBits(5))
+	fmt.Println(countBits2(5))
 }
