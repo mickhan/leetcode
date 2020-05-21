872068bd06bc46f6114f7fac1ed1ce499b33e4cd
mickhan <catmic27@gmail.com>
2020-05-21 11:36:07 +0800

    改进

--- reverse-linked-list/main.go
+++ reverse-linked-list/main.go
@@ -27,6 +27,34 @@ func reverseList(head *ListNode) *ListNode {
 	}
 }
 
+// 递归
+func reverseList2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	} else {
+		n := reverseList(head.Next)
+		head.Next.Next = head // head.Next 就是n的最后一个元素，所以不需要再遍历一遍找了
+		head.Next = nil
+		return n
+	}
+}
+
+// 迭代
+func reverseList3(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var cur, pre, t *ListNode
+	cur = head
+	for cur != nil {
+		t = cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = t
+	}
+	return pre
+}
+
 func printList(head *ListNode) {
 	if head == nil {
 		return
@@ -47,5 +75,5 @@ func main() {
 	}
 	n1.Next = &n2
 	n2.Next = &n3
-	printList(reverseList(&n1))
+	printList(reverseList3(&n1))
 }
