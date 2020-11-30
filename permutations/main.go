e8475fd1744cde23163b560b4dada32d6786f2ea
mickhan <catmic27@gmail.com>
2020-11-30 10:31:09 +0800

    整理

--- permutations/main.go
+++ permutations/main.go
@@ -7,14 +7,18 @@ var gnums []int // 全集
 var cur []int   // 当前尝试
 
 func makeOptions(i int, options []int) []int {
+	// 避免每层递归的options被修改，拷贝一份新的出来
+	opt := make([]int, len(options))
+	copy(opt, options)
 	// 剔除第i个，形成新的可选项
-	ret := append(options[:i], options[i+1:]...)
+	ret := append(opt[:i], opt[i+1:]...)
 	return ret
 }
 
 func backtrace(options []int) {
 	// 当前尝试的长度等于全集，说明排列完成，记录到结果中
 	if len(cur) == len(gnums) {
+		// 直接append是cur的引用，会被修改，拷贝一份出来
 		tmp := make([]int, len(cur))
 		copy(tmp, cur)
 		sets = append(sets, tmp)
@@ -22,9 +26,7 @@ func backtrace(options []int) {
 	// 循环尝试可选项中的所有可能性
 	for i := 0; i < len(options); i++ {
 		cur = append(cur, options[i])
-		opt := make([]int, len(options))
-		copy(opt, options)
-		backtrace(makeOptions(i, opt))
+		backtrace(makeOptions(i, options))
 		cur = cur[:len(cur)-1]
 	}
 }
